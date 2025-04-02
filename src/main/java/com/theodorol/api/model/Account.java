0e118cf4537b2f9432276907731545e93ea1a8d3
Lucas <lucastheodoromourofernandes@gmail.com>
2025-04-02 13:47:24 -0400

    Hotfix 'precision' e 'scale'

--- src/main/java/com/theodorol/api/model/Account.java
+++ src/main/java/com/theodorol/api/model/Account.java
@@ -14,7 +14,7 @@ public class Account {
 
     private String agency;
 
-    @Column(precision = 2, scale = 13)
+    @Column(precision = 13, scale = 2)
     private BigDecimal balance;
 
     @Column(name = "account_limit", precision = 2, scale = 13)
--- src/main/java/com/theodorol/api/model/Card.java
+++ src/main/java/com/theodorol/api/model/Card.java
@@ -11,7 +11,7 @@ public class Card {
     @Column(unique = true)
     private String number;
 
-    @Column( name = "limit_card", precision = 2, scale = 13)
+    @Column( name = "limit_card", precision = 13, scale = 2)
     private BigDecimal limit;
 
     // Getters e setters
