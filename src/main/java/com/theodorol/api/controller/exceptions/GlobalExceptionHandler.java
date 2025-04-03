e428e838fc9637ffaac6c622fbc9851124bfab2d
Lucas <lucastheodoromourofernandes@gmail.com>
2025-04-02 21:10:36 -0400

    Tratamento de Exceções em APIs REST

---
+++ src/main/java/com/theodorol/api/controller/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.theodorol.api.controller.exceptions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    private Logger logger  = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handlerBusinessException(IllegalArgumentException businessException){
+        return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handlerNotFoundException(NoSuchElementException notFoundException){
+        return new ResponseEntity<>("cliente não encontrado", HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handlerUnexpectedException(Throwable unexpectedException){
+        var message = "Erro não esperado";
+        logger.error(message, unexpectedException);
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
